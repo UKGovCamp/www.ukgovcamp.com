b06c37b9a93252e22a949e57828fdc7697b3ef9c
Joe Roberts <joe@engiqueer.net>
2025-07-05 09:47:08 +0100

    Add basic post views

---
+++ src/content.config.ts
@@ -0,0 +1,32 @@
+import { defineCollection, reference, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const authors = defineCollection({
+  loader: glob({
+    pattern: "*.yaml",
+    base: "./authors",
+  }),
+  schema: z.object({
+    name: z.string(),
+    pronouns: z.string().optional(),
+  }),
+});
+
+const wordpressExport = defineCollection({
+  loader: glob({
+    pattern: "**/*.md",
+    base: "./wordpress-export",
+    generateId: ({ entry, data }) => {
+      const date = data.date as Date;
+      if (!date) return entry.replace(/\/index\.md/, "");
+      return `${date.getFullYear()}/${String(date.getMonth() + 1).padStart(2, "0")}/${String(date.getDate()).padStart(2, "0")}/${entry.replace(/\/index\.md/, "")}`;
+    },
+  }),
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    author: reference("authors"),
+  }),
+});
+
+export const collections = { wordpressExport, authors };
